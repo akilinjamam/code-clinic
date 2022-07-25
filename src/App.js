01d7c195ca856d39cf0270c5cbfca97d7d2daf0b
akilinjamam <akilinjamam@gmail.comm>
2022-07-25 19:15:57 +0600

    file-updated 26-07-22

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to code clinic</h1>
+      <h2>please use git pull command in your vs code terminal to get update file in your local computer which was updated by other developers</h2>
       <br />
       <button class="btn btn-primary">Daisy Apa</button>
     </div>
