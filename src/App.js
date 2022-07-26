ae2536bad86ea112e2d8fc34503c2833487eb8b8
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-07-26 12:40:44 +0600

    new branch created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
       <h1>Welcome to code clinic</h1>
       <h2>please use git pull command in your vs code terminal to get update file in your local computer which was updated by other developers</h2>
       <br />
-      <button class="btn btn-primary">Daisy Apa</button>
+      <button class="btn btn-primary">Daisy Apaa</button>
     </div>
   );
 }
