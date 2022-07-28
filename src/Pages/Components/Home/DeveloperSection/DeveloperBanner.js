9873923539d0632a2856cf30885e75b94b9561e2
alamindjp <alaminhossaindjp@gmail.com>
2022-07-28 17:37:05 +0600

    responsive banner

--- src/Pages/Components/Home/DeveloperSection/DeveloperBanner.js
+++ src/Pages/Components/Home/DeveloperSection/DeveloperBanner.js
@@ -6,8 +6,8 @@ const DeveloperBanner = () => {
     return (
         <div class="hero mt-10">
             <div class="hero-content flex-col lg:flex-row">
-                <div class="flex sm:items-center flex-col lg:flex-row">
-                    <div class="grid flex-grow w-[50%] card rounded-box place-items-center">
+                <div class="flex items-center flex-col lg:flex-row">
+                    <div class="grid flex-grow sm:w-[50%] card rounded-box place-items-center">
                         <img className='w-[80px]' src={CoddingSkills} alt="CoddingSkills" />
                         <h3 class="text-lg font-bold text-[#CFD2CF] mt-6">FOR DEVELOPERS</h3>
                         <h3 class="text-4xl font-bold text-slate-700">Improve your coding skills.</h3>
@@ -15,7 +15,7 @@ const DeveloperBanner = () => {
                         <button class="btn btn-primary">Try for free challenge</button>
                     </div>
                     <div class="divider lg:divider-horizontal"></div>
-                    <div class="grid flex-grow w-[50%] card rounded-box place-items-center">
+                    <div class="grid flex-grow sm:w-[50%] card rounded-box place-items-center">
                         <img className='w-[70px]' src={InterviewImg} alt="InterviewImg" />
                         <h3 class="text-lg font-bold text-[#CFD2CF] mt-1">FOR INTERVIEWER</h3>
                         <h3 class="text-4xl font-bold lg:text-sky-800">Interview and evaluate candidates.</h3>
