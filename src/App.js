2af2bf1359c8454d890793c554e0954b1698544b
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-08-03 00:20:55 +0600

    courses component created

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import ContactUs from "./Pages/Contact/ContactUs";
 import Learn from "./Pages/Learnlanguage/Learn";
 import Language from "./Pages/Learnlanguage/Component/Language";
 import NotFound from "./Pages/SharedPage/NotFound/NotFound";
+import Courses from "./Pages/Components/Courses/Courses";
+import HtmlCourse from "./Pages/Components/Courses/HtmlCourse";
+import JavascriptCourse from "./Pages/Components/Courses/JavascriptCourse";
 
 function App() {
   return (
@@ -24,6 +27,13 @@ function App() {
         <Route path="/quiz" element={<Quiz />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signUp" element={<Signup />}></Route>
+
+        <Route path="/courses" element={<Courses/>}>
+          
+        <Route index element={<HtmlCourse/>}></Route>
+        <Route path="/courses/javascriptcourse" element={<JavascriptCourse/>}></Route>
+        </Route>
+
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
       <Footer />
---
+++ src/Pages/Components/Courses/HtmlCourse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HtmlCourse = () => {
+    return (
+        <div>
+            <h2 className='text-4xl text-primary'>HTML Course</h2>
+        </div>
+    );
+};
+
+export default HtmlCourse;
