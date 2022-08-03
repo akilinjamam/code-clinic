708df9d6a6b296d16c9c75a8a874e11a5e5ae060
akilinjamam <akilinjamam@gmail.comm>
2022-08-03 14:41:05 +0600

    userName added after login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Pages/Components/Home/Home";
 import Quiz from "./Pages/Components/Home/QuizSection/Quiz";
 import Login from "./Pages/Components/user/Login";
 import Signup from "./Pages/Components/user/Signup";
+import ContactUs from "./Pages/Contact/ContactUs"
 import Footer from "./Pages/SharedPage/Footer/Footer";
 import NotFound from "./Pages/SharedPage/NotFound/NotFound";
 import Courses from "./Pages/Components/Courses/Courses";
@@ -21,14 +22,15 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signUp" element={<Signup />}></Route>
 
-        <Route path="/courses" element={<Courses/>}>
-          
-        <Route index element={<HtmlCourse/>}></Route>
-        <Route path="/courses/javascriptcourse" element={<JavascriptCourse/>}></Route>
-        <Route path="/courses/c++" element={<Cpp/>}></Route>
-        </Route>
+        <Route path="/courses" element={<Courses />}>
 
-        <Route path="*" element={<NotFound/>}></Route>
+          <Route index element={<HtmlCourse />}></Route>
+          <Route path="/courses/javascriptcourse" element={<JavascriptCourse />}></Route>
+          <Route path="/courses/c++" element={<Cpp />}></Route>
+        </Route>
+        <Route path="/contactUs" element={<ContactUs></ContactUs>}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer />
     </div>
