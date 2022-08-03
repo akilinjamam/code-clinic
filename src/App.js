4c03d7278633c98e3003caf0683d19bb287b63a9
MD. ALAMIN HOSSAIN <96736699+alamindjp@users.noreply.github.com>
2022-08-03 10:05:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import Quiz from "./Pages/Components/Home/QuizSection/Quiz";
 import Login from "./Pages/Components/user/Login";
 import Signup from "./Pages/Components/user/Signup";
 import Footer from "./Pages/SharedPage/Footer/Footer";
-import About from "./Pages/Components/About";
-import ContactUs from "./Pages/Contact/ContactUs";
-import Learn from "./Pages/Learnlanguage/Learn";
-import Language from "./Pages/Learnlanguage/Component/Language";
 import NotFound from "./Pages/SharedPage/NotFound/NotFound";
 import Courses from "./Pages/Components/Courses/Courses";
 import HtmlCourse from "./Pages/Components/Courses/HtmlCourse/HtmlCourse";
@@ -21,10 +17,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact_us" element={<ContactUs />}></Route>
-        <Route path="/learn" element={<Learn />}></Route>
-        <Route path="/learn/:Language" element={<Language />}></Route>
         <Route path="/quiz" element={<Quiz />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signUp" element={<Signup />}></Route>
