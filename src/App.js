f7302f5d40961c36d1462cb955aa49e67f4b88ea
alamindjp <alaminhossaindjp@gmail.com>
2022-08-18 23:52:24 +0600

    create and modify my course in user dashboard

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import HtmlStyle from "./Pages/Components/Courses/HtmlCourse/HtmlStyle";
 import HtmlImage from "./Pages/Components/Courses/HtmlCourse/HtmlImage";
 import HtmlCce from "./Pages/Components/Courses/HtmlCourse/HtmlCce";
 import Editor from "./Pages/Components/Editor/Editor";
-import PaidCourse from "./Pages/Components/Home/PaidCourse/PaidCourse";
 import InterviewPrep from "./Pages/Components/Home/CompilerHome/InterviewPrep/InterviewPrep";
 import JsCourses from "./Pages/Components/Courses/JsCourses";
 import JsSyntex from "./Pages/Components/Courses/JavascriptCourse/JsSyntex";
@@ -32,7 +31,7 @@ import JavascriptQa from "./Pages/Components/Home/CompilerHome/InterviewPrep/Jav
 import HtmlQa from "./Pages/Components/Home/CompilerHome/InterviewPrep/HtmlQa";
 import UserProfile from "./Pages/UserDashboard/UserProfile";
 import Profile from "./Pages/UserDashboard/Profile";
-import MyCourse from "./Pages/UserDashboard/MyCourse";
+import BuyCourse from "./Pages/UserDashboard/BuyCourse";
 
 
 function App() {
@@ -48,12 +47,13 @@ function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/Editor" element={<Editor />}></Route>
         <Route path="/quiz" element={<Quiz />}></Route>
-        <Route path="/pythonCom" element={<PythonCompiler />}></Route>
+
+
         <Route path="/introduction" element={<Introduction />}></Route>
 
-        {/* Free Course Routes */}
-
         <Route path="/courses" element={<Courses />}>
+          <Route index element={<Intro />}></Route>
+
           <Route
             path="/courses/intro"
             element={<Intro />}
@@ -78,22 +78,13 @@ function App() {
             path="/courses/htmlCce"
             element={<HtmlCce />}
           />
-
-
-
           <Route
             path="/courses/cpp"
             element={<Cpp />}
           />
-          <Route index element={<Intro />}></Route>
-          <Route path="/courses/intro" element={<Intro />} />
-          <Route path="/courses/attribute" element={<Attributes />} />
-          <Route path="/courses/htmlStyle" element={<HtmlStyle />} />
-          <Route path="/courses/htmlImage" element={<HtmlImage />} />
-          <Route path="/courses/htmlCce" element={<HtmlCce />} />
-          <Route path="/courses/cpp" element={<Cpp />} />
-
         </Route>
+        {/* Interview Preparation Section Routes */}
+
         <Route path="/interview-prep" element={<InterviewPrep />}>
           {/* <Route index element={<Intro />}></Route> */}
 
@@ -106,15 +97,6 @@ function App() {
             path="/interview-prep/html"
             element={<HtmlQa />}
           />
-         {/* Ends of Free Course Routes */}
-
-        {/* Interview Preparation Section Routes */}
-
-        <Route path="/interview-prep" element={<InterviewPrep />}>
-
-          <Route path="/interview-prep/javascript" element={<JavascriptQa />} />
-          <Route path="/interview-prep/html" element={<HtmlQa />} />
-
         </Route>
 
         {/* Ends of Interview Preparation Section Routes */}
@@ -145,13 +127,6 @@ function App() {
             element={<JsConst />}
           />
 
-          <Route index element={<JavascriptCourse />} />
-          <Route path="/jsCourses/jsSyntex" element={<JsSyntex />} />
-          <Route path="/jsCourses/jsVariables" element={<JsVariables />} />
-          <Route path="/jsCourses/jsLet" element={<JsLet />} />
-          <Route path="/jsCourses/jsConst" element={<JsConst />} />
-
-
         </Route>
 
         <Route path="/cplusPlusCourses" element={<CplusplusCourses />}>
@@ -161,19 +136,20 @@ function App() {
             element={<CppIntro />}
           />
         </Route>
+
+        <Route path="*" element={<NotFound />} />
+
         {/* User Profile route  */}
 
         <Route path="/dashboard" element={<UserProfile />}>
           <Route index element={<Profile />}></Route>
+
           <Route
-            path="/dashboard/my_course"
-            element={<MyCourse />}
+            path="/dashboard/education"
+            element={<BuyCourse />}
           />
         </Route>
-
-        <Route path="*" element={<NotFound />} />
       </Routes>
-
       <Footer />
     </div>
   );
